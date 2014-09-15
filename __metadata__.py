1e2da99b8b81c019cbe4f3865456d3c7293aa4df
Trevor Bača <trevorbaca@gmail.com>
2014-09-14 20:41:51 -0400

    Added top-level directories.

---
+++ __metadata__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+metadata = collections.OrderedDict([
+    ('catalog_number', 'AWN-013'),
+    ('forces_tagline', 'for cello'),
+    ('paper_dimensions', '11 x 17 in'),
+    ('price', '\\euro 22 / \\$28'),
+    ('title', 'Huitzil'),
+    ('year', 2014),
+    ])
