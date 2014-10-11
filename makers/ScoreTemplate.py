07786e5efa3fa41ca6b3324d0406990cf1140380
Trevor Bača <trevorbaca@gmail.com>
2014-10-11 14:33:14 -0400

    Built score template.

---
+++ makers/ScoreTemplate.py
@@ -0,0 +1,69 @@
+from abjad import *
+
+
+class ScoreTemplate(abctools.AbjadValueObject):
+    r'''Huitzil score template.
+    '''
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self):
+        r'''Calls Huitzil score template.
+
+        >>> from huitzil import makers
+        >>> template = makers.ScoreTemplate()
+        >>> score = template()
+        
+        >>> f(score)
+        \context Score = "Score" <<
+            \context TimeSignatureContext = "Time Signature Context" {
+            }
+            \context Staff = "Staff" <<
+                \clef "bass"
+                \context TextSpannerVoice = "Text Spanner Voice" {
+                }
+                \context Voice = "Music Voice" {
+                }
+            >>
+        >>
+
+        Returns score.
+        '''
+
+        # make time signature context
+        time_signature_context = scoretools.Context(
+            context_name='TimeSignatureContext',
+            name='Time Signature Context',
+            )
+
+        # make staff
+        staff = Staff(
+            name='Staff',
+            )
+        staff.is_simultaneous = True
+        attach(Clef('bass'), staff)
+
+        # make text spanner voice
+        text_spanner_voice = Voice(
+            context_name='TextSpannerVoice', 
+            name='Text Spanner Voice',
+            )
+        staff.append(text_spanner_voice)
+
+        # make music voice
+        music_voice = Voice(
+            name='Music Voice',
+            )
+        staff.append(music_voice)
+
+        # make score
+        score = Score(
+            [
+            time_signature_context,
+            staff,
+            ],
+            name='Score',
+            )
+        
+        # return score
+        return score
