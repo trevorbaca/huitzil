9924d534f45dfa4fda458734aa913cedd482d0b8
Trevor Bača <trevorbaca@gmail.com>
2014-12-15 16:58:55 -0500

    Reworked flight.

--- segments/abbreviations.py
+++ segments/abbreviations.py
@@ -12,6 +12,7 @@ dashed_arrow = indicatortools.Arrow(
     dash_period=1.5,
     left_broken_text=False,
     left_hspace=0.5,
+    right_broken_arrow=False,
     right_broken_padding=0,
     right_padding=1.75,
     )
