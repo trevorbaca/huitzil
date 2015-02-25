499bc2245b3306501755c774bcae6c28557804a6
Trevor Bača <trevorbaca@gmail.com>
2015-02-25 14:30:40 -0500

    Versioned segments.

---
+++ segments/flight_f/versions/definition_0003.py
@@ -0,0 +1,148 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from huitzil import makers
+from huitzil import materials
+from huitzil.segments.abbreviations import *
+
+
+segment_maker = makers.FlightSegmentMaker(
+    markup_leaves=False,
+    name='flight F',
+    pitches=[
+        (0, 'A2'),
+        (20, 'G2'),
+        (24, 'G2()'),
+        (28, 'A1'),
+        ],
+    staff_line_count=11
+    )
+
+segment_maker.lh_glissandi = [
+    (0, 1),
+    (2, 3),
+    ]
+
+segment_maker.notes = [
+    (11, '1/1', fz),
+    bar,
+    (11, '3/4', None),
+    bar,
+    (10, '3/4', fz),
+    bar,
+    (10, '1/1', None),
+    bar,
+    (9, '1/1', fz),
+    bar,
+    (9, '3/4', None),
+    bar,
+    (8, '3/4', fz),
+    bar,
+    (8, '1/1', None),
+    bar,
+    (7, '1/1', fz),
+    bar,
+    (7, '3/4', None),
+    bar,
+    (6, '3/4', fz),
+    bar,
+    (6, '1/1', None),
+    bar,
+    (5, '1/1', fz),
+    bar,
+    (5, '3/4', None),
+    bar,
+    (4, '3/4', fz),
+    bar,
+    (4, '1/1', None),
+    bar,
+    (3, '1/1', fz),
+    bar,
+    (3, '3/4', None),
+    bar,
+    (2, '3/4', fz),
+    bar,
+    (2, '1/1', None),
+    bar,
+    # G2
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', fffz),
+    bar,
+    # descent
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', None),
+    bar,
+    (1, '1/1', None),
+    bar,
+    (1, '1/1', None),
+    bar,
+    # A2
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', fffz),
+    bar,
+    (1, '1/1', fffz),
+    bar,
+    ]
+
+segment_maker.glissando_break_indices = [
+    ]
+
+segment_maker.tempo_map = [
+    (0, Accelerando()),
+    (3, quarter_88),
+    (4, quarter_66),
+    (4, Accelerando()),
+    (7, quarter_88),
+    (8, quarter_66),
+    (8, Accelerando()),
+    (11, quarter_88),
+    (12, quarter_66),
+    (12, Accelerando()),
+    (15, quarter_88),
+    (16, quarter_66),
+    (16, Accelerando()),
+    (19, quarter_88),
+    (24, quarter_66),
+    (24, Accelerando()),
+    (27, quarter_88),
+    (28, Ritardando()),
+    (31, quarter_66),
+    ]
+
+segment_maker.tremolo_map = [
+    ]
+
+segment_maker.underlying_dynamics = [
+    (0, p),
+    (0, crescendo),
+    (3, mf),
+    (3, decrescendo),
+    (4, p),
+    (4, crescendo),
+    (7, mf),
+    (7, decrescendo),
+    (8, p),
+    (8, crescendo),
+    (11, mf),
+    (11, decrescendo),
+    (12, p),
+    (12, crescendo),
+    (15, mf),
+    (15, decrescendo),
+    (16, p),
+    (16, crescendo),
+    (22, ff),
+    (24, mf),
+    (24, crescendo),
+    (28, ff),
+    (28, decrescendo),
+    (31, pp),
+    ]
