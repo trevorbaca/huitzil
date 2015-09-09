feddeb38c0154d4103dddd7ca97c3332fe23e2bd
Trevor Bača <trevorbaca@gmail.com>
2015-09-09 15:17:06 -0400

    Updated tests.

--- huitzil/test/test_segments.py
+++ huitzil/test/test_segments.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-import ide
 import os
 import pytest
 import shutil
 import huitzil
+import ide
 from abjad.tools import systemtools
 
 
-boilerplate_path = ide.tools.idetools.AbjadIDEConfiguration().abjad_ide_boilerplate_directory
+configuration = ide.tools.idetools.AbjadIDEConfiguration()
+boilerplate_path = configuration.abjad_ide_boilerplate_directory
 boilerplate_path = os.path.join(boilerplate_path, '__illustrate_segment__.py')
 
 segments_path = os.path.join(huitzil.__path__[0], 'segments')
@@ -18,9 +19,11 @@ directory_names = [_ for _ in directory_names if not _.startswith(('.', '_'))]
 segment_paths = [os.path.join(segments_path, _) for _ in directory_names]
 segment_paths = [_ for _ in segment_paths if os.path.isdir(_)]
 
-
 @pytest.mark.parametrize('segment_path', segment_paths)
-def test_huitzil_segments_01(segment_path):
+def test_segments_01(segment_path):
+    r'''Illustrates segments.
+    '''
+    
     local_boilerplate_path = os.path.join(
         segment_path,
         '__illustrate_segment__.py',
@@ -41,23 +44,27 @@ def test_huitzil_segments_01(segment_path):
         segment_path,
         'illustration.candidate.pdf',
         )
+
     if os.path.exists(local_boilerplate_path):
         os.remove(local_boilerplate_path)
+
     with systemtools.FilesystemState(
         keep=[illustration_ly_path, illustration_pdf_path],
         remove=[local_boilerplate_path],
         ):
         shutil.copyfile(boilerplate_path, local_boilerplate_path)
+        ide.tools.idetools.AbjadIDE._replace_in_file(
+            local_boilerplate_path,
+            'PREVIOUS_SEGMENT_METADATA_IMPORT_STATEMENT',
+            'previous_segment_metadata = None',
+            )
         assert os.path.exists(local_boilerplate_path)
         assert not os.path.exists(illustration_candidate_ly_path)
         assert not os.path.exists(illustration_candidate_pdf_path)
-
         command = 'python {}'.format(local_boilerplate_path)
         exit_status = systemtools.IOManager.spawn_subprocess(command)
         assert exit_status == 0
-
         assert os.path.exists(illustration_candidate_ly_path)
         assert os.path.exists(illustration_candidate_pdf_path)
-
         shutil.move(illustration_candidate_ly_path, illustration_ly_path)
         shutil.move(illustration_candidate_pdf_path, illustration_pdf_path)
