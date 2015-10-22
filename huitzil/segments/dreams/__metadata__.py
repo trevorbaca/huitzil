d4b03979ac73b1c3d8698da2558a9e243e7a47c6
Trevor Bača <trevorbaca@gmail.com>
2015-10-21 22:25:04 -0400

    Updated tests.

--- huitzil/segments/dreams/__metadata__.py
+++ huitzil/segments/dreams/__metadata__.py
@@ -3,9 +3,5 @@ from abjad import *
 
 
 metadata = datastructuretools.TypedOrderedDict(
-    [
-        ('first_bar_number', 1),
-        ('segment_count', 10),
-        ('segment_number', 1),
-        ]
+    []
     )
