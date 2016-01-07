b17659f26cfb0e40480e73be0574bf528302f0e2
Trevor Bača <trevorbaca@gmail.com>
2016-01-07 16:54:21 -0600

    Udpated specifiers.

--- huitzil/tools/DreamsSegmentMaker.py
+++ huitzil/tools/DreamsSegmentMaker.py
@@ -572,7 +572,7 @@ class DreamsSegmentMaker(makertools.SegmentMaker):
         message = message.format(voice_name, stage)
         raise KeyError(message)
 
-    def make_music_handler(
+    def make_specifiers(
         self, 
         scope, 
         specifiers,
