b25dafc5e8c94806be51f14466fb47eca57d1a44
Trevor Bača <trevor.baca@gmail.com>
2016-07-24 17:09:28 -0500

    Updated metadata.

--- huitzil/__metadata__.py
+++ huitzil/__metadata__.py
@@ -5,7 +5,7 @@ import collections
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-013'),
     ('forces_tagline', 'for cello'),
-    ('paper_dimensions', '17 x 11 in'),
+    ('paper_size', '17 x 11 in'),
     ('price', '\\euro 22 / \\$28'),
     ('title', 'Huitzil'),
     ('year', 2014),
