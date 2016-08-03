db1680d87fb050f3d70d806540d917741cd337f5
Trevor Bača <trevor.baca@gmail.com>
2016-08-03 10:41:34 -0500

    Updated catalog number.

--- huitzil/__metadata__.py
+++ huitzil/__metadata__.py
@@ -3,7 +3,7 @@ import collections
 
 
 metadata = collections.OrderedDict([
-    ('catalog_number', 'AWN-013'),
+    ('catalog_number', 'AWN-009'),
     ('forces_tagline', 'for cello'),
     ('paper_size', '17 x 11 in'),
     ('price', '\\euro 22 / \\$28'),
