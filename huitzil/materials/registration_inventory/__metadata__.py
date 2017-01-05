7a32a48ab38230ef98e80fddd76891c6c03e8b85
Trevor Bača <trevor.baca@gmail.com>
2017-01-05 12:13:32 -0600

    Cleaned up metadata.

--- huitzil/materials/registration_inventory/__metadata__.py
+++ huitzil/materials/registration_inventory/__metadata__.py
@@ -2,6 +2,4 @@
 import abjad
 
 
-metadata = abjad.datastructuretools.TypedOrderedDict([
-    ('output_material_class_name', 'RegistrationInventory'),
-    ])
+metadata = abjad.datastructuretools.TypedOrderedDict()
