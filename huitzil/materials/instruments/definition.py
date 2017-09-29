54d227f1860dcbd509c9bf78590a2a505b29b03f
Trevor Bača <trevor.baca@gmail.com>
2017-09-29 10:04:55 -0500

    Cleaned up instruments.

--- huitzil/materials/instruments/definition.py
+++ huitzil/materials/instruments/definition.py
@@ -5,7 +5,7 @@ import baca
 instruments = abjad.TypedOrderedDict([
     (
         'cello',
-        abjad.instrumenttools.Cello(
+        abjad.Cello(
             name_markup=baca.markup.instrument('Cello'),
             pitch_range='[A1, G5]',
             short_name_markup=baca.markup.short_instrument('Vc.'),
