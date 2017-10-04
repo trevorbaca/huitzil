87d312f74f2f43b8527f0dadd22a2b826cb9ba13
Trevor Bača <trevor.baca@gmail.com>
2017-10-04 12:51:33 -0500

    Rerendered segments.

--- huitzil/segments/_/__metadata__.py
+++ huitzil/segments/_/__metadata__.py
@@ -1,10 +1,4 @@
 import abjad
 
 
-metadata = abjad.TypedOrderedDict(
-    [
-        ('first_bar_number', 1),
-        ('segment_count', 10),
-        ('segment_number', 1),
-        ]
-    )
+metadata = abjad.TypedOrderedDict()
