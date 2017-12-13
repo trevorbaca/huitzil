2970a59f72b6340eabc6f5b251188db026f5b839
Trevor Bača <trevor.baca@gmail.com>
2017-12-13 11:38:24 -0600

    Rerendered materials.

--- huitzil/materials/metronome_marks/definition.py
+++ huitzil/materials/metronome_marks/definition.py
@@ -4,22 +4,22 @@ import abjad
 metronome_marks = abjad.MetronomeMarkDictionary([
     (
         '44',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 44),
+        abjad.MetronomeMark((1, 4), 44),
         ),
     (
         '52',
-        abjad.MetronomeMark(abjad.Duration(1, 8), 52),
+        abjad.MetronomeMark((1, 8), 52),
         ),
     (
         '66',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 66),
+        abjad.MetronomeMark((1, 4), 66),
         ),
     (
         '78',
-        abjad.MetronomeMark(abjad.Duration(1, 8), 78),
+        abjad.MetronomeMark((1, 8), 78),
         ),
     (
         '88',
-        abjad.MetronomeMark(abjad.Duration(1, 4), 88),
+        abjad.MetronomeMark((1, 4), 88),
         ),
     ])
