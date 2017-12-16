b461efe2d5d754890faa280f4534e889ab24b82a
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 15:44:56 -0600

    Cleaning up metadata.

--- huitzil/segments/_/__metadata__.py
+++ huitzil/segments/_/__metadata__.py
@@ -3,7 +3,7 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
-        ('first_bar_number', 1),
+        ('first_measure_number', 1),
         ('segment_count', 10),
         ('segment_number', 1),
         ]
