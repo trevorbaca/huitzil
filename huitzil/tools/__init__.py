a983b5097bcda83c9c7fc19f90c5fdbea62ea6a7
Trevor Bača <trevor.baca@gmail.com>
2018-07-04 16:17:10 -0500

    Cleaned up text spanners.

--- huitzil/tools/__init__.py
+++ huitzil/tools/__init__.py
@@ -4,5 +4,4 @@ from .DreamsSegmentMaker import DreamsSegmentMaker
 from .FlightScoreTemplate import FlightScoreTemplate
 from .FlightSegmentMaker import FlightSegmentMaker
 from .MarkupLibrary import MarkupLibrary
-from .dashed_arrow import dashed_arrow
 from .final_markup import final_markup
