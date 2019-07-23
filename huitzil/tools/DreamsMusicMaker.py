a4d1f8d1889891ae384e01456cbbd22953ead213
Trevor Bača <trevor.baca@gmail.com>
2019-07-23 11:50:53 -0400

    Cleaned up rhythm construction.

--- huitzil/tools/DreamsMusicMaker.py
+++ huitzil/tools/DreamsMusicMaker.py
@@ -464,8 +464,8 @@ class DreamsMusicMaker(object):
                 fraction = fraction.with_denominator(128)
                 numerators.append(fraction.numerator)
             ratio = abjad.Ratio(numerators)
-            maker = rmakers.tuplet(
-                [ratio],
+            maker = rmakers.rhythm(
+                rmakers.tuplet([ratio]),
                 rmakers.rewrite_dots(),
                 rmakers.rewrite_sustained(),
                 rmakers.force_diminution(),
