48057682489a6560f1f0a0e46a08bae4d57ab400
Trevor Bača <trevor.baca@gmail.com>
2021-07-16 09:11:59 -0400

    Cleaned up definition files.

--- huitzil/segments/06/definition.py
+++ huitzil/segments/06/definition.py
@@ -40,7 +40,6 @@ maker = baca.SegmentMaker(
     clock_time_extra_offset=(0, 13),
     segment_directory=baca.Path(__file__).resolve().parent,
     time_signatures=time_signatures,
-    validate_measure_count=23,
 )
 
 maker(
