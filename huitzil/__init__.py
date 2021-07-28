8818cb67f3b59ee3ca559a070609ef353cf06944
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 17:38:05 -0400

    Cleaned up package initializer.

--- huitzil/__init__.py
+++ huitzil/__init__.py
@@ -1,22 +1,6 @@
 import sys
 
-from .library import (
-    DreamsMusicMaker,
-    ScoreTemplate,
-    instruments,
-    metronome_marks,
-    pitch_classes,
-    registrations,
-)
-
-__all__ = [
-    "ScoreTemplate",
-    "DreamsMusicMaker",
-    "instruments",
-    "metronome_marks",
-    "pitch_classes",
-    "registrations",
-]
+from . import library
 
 if sys.version_info[:2] < (3, 9):
     raise ImportError("Requires Python 3.9 or later")
