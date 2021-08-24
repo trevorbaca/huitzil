0d08aca7810850b05e8c4e58027c49673e53bb6c
Trevor Bača <trevor.baca@gmail.com>
2021-08-24 15:03:33 -0400

    Cleaned up layout files.

--- huitzil/segments/02/layout.py
+++ huitzil/segments/02/layout.py
@@ -4,13 +4,11 @@ top = 17
 staves = (10, 15)
 separation = 57
 
-spacing = baca.SpacingSpecifier(
-    breaks=baca.breaks(
-        baca.page(
-            1,
-            baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
-            baca.system(measure=16, y_offset=top + 1 * separation, distances=staves),
-        ),
+spacing = baca.make_layout(
+    baca.page(
+        1,
+        baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
+        baca.system(measure=16, y_offset=top + 1 * separation, distances=staves),
     ),
     fallback_duration=(1, 20),
 )
