0c6a0c4304db67b4ecdab79db94d515b748f03c3
Trevor Bača <trevor.baca@gmail.com>
2022-02-23 16:16:03 +0100

    Cleaned up library.

--- huitzil/segments/01/music.py
+++ huitzil/segments/01/music.py
@@ -5,191 +5,188 @@ from abjadext import rmakers
 from huitzil import library
 
 
-class Maker:
-    def __init__(
-        self,
-        extra_counts,
-        segment_lists,
-        voice_map,
-        pc_displacement=(),
-        pc_operators=(),
-    ):
-        self.extra_counts = extra_counts
-        self.segment_lists = tuple(segment_lists)
-        self.voice_map = voice_map
-        self.pc_displacement = pc_displacement or []
-        self.pc_operators = pc_operators or []
+def make_music(
+    extra_counts,
+    segment_lists,
+    voice_map,
+    pc_displacement=(),
+    pc_operators=(),
+):
+    tuplets = _make_rhythm(segment_lists, voice_map, extra_counts, pc_operators)
+    assert all(isinstance(_, abjad.Tuplet) for _ in tuplets)
+    _displace_pitch_classes(tuplets, pc_displacement)
+    _register_voices(tuplets)
+    _attach_beams(tuplets)
+    assert all(isinstance(_, abjad.Tuplet) for _ in tuplets)
+    return tuplets
 
-    def __call__(self) -> abjad.Selection:
-        tuplets = self._make_rhythm()
-        assert all(isinstance(_, abjad.Tuplet) for _ in tuplets)
-        self._displace_pitch_classes(tuplets)
-        self._register_voices(tuplets)
-        self._attach_beams(tuplets)
-        assert all(isinstance(_, abjad.Tuplet) for _ in tuplets)
-        return tuplets
 
-    def _attach_beams(self, music):
-        tuplets = abjad.iterate.components(music, abjad.Tuplet)
-        for tuplet in tuplets:
-            voice_numbers = [abjad.get.indicator(_, int) for _ in tuplet]
-            runs = abjad.sequence.group_by(voice_numbers)
-            counts = [len(_) for _ in runs]
-            note_groups = abjad.sequence.partition_by_counts(tuplet[:], counts)
-            for note_group in note_groups:
-                note_group = abjad.Selection(note_group)
-                abjad.beam(note_group)
+def _attach_beams(music):
+    tuplets = abjad.iterate.components(music, abjad.Tuplet)
+    for tuplet in tuplets:
+        voice_numbers = [abjad.get.indicator(_, int) for _ in tuplet]
+        runs = abjad.sequence.group_by(voice_numbers)
+        counts = [len(_) for _ in runs]
+        note_groups = abjad.sequence.partition_by_counts(tuplet[:], counts)
+        for note_group in note_groups:
+            note_group = abjad.Selection(note_group)
+            abjad.beam(note_group)
 
-    def _attach_voice_numbers(self, note_lists):
-        for component in self.voice_map:
-            assert len(component) == 2
-            voice_number = component[0]
-            indices = component[1]
-            notes = abjad.sequence.flatten(note_lists)
-            for i, note in enumerate(notes):
-                if i in indices:
-                    abjad.detach(int, note)
-                    abjad.attach(voice_number, note)
+
+def _attach_voice_numbers(note_lists, voice_map):
+    for component in voice_map:
+        assert len(component) == 2
+        voice_number = component[0]
+        indices = component[1]
         notes = abjad.sequence.flatten(note_lists)
-        for note in notes:
-            assert abjad.get.has_indicator(note, int), repr(note)
-
-    def _displace_pitch_classes(self, music):
-        if not self.pc_displacement:
-            return
-        notes = abjad.Selection(music).leaves(pitched=True)
-        total_notes = len(notes)
         for i, note in enumerate(notes):
-            register = None
-            for pattern in self.pc_displacement:
-                if pattern.matches_index(i, total_notes):
-                    register = "high"
-                    break
-            else:
-                register = "low"
-            if register == "high":
-                pass
-            elif register == "low":
-                source_pitch = note.written_pitch
-                transposed_pitch = source_pitch.transpose(n=-12)
-                note.written_pitch = transposed_pitch
-            else:
-                raise ValueError(register)
+            if i in indices:
+                abjad.detach(int, note)
+                abjad.attach(voice_number, note)
+    notes = abjad.sequence.flatten(note_lists)
+    for note in notes:
+        assert abjad.get.has_indicator(note, int), repr(note)
 
-    def _make_inner_tuplets(self, note_lists):
-        extra_counts = self.extra_counts or [0]
-        extra_counts = abjad.CyclicTuple(extra_counts)
-        inner_tuplets = []
-        for i, note_list in enumerate(note_lists):
-            start_duration = sum(_.written_duration for _ in note_list)
-            extra_count = extra_counts[i]
-            extra_duration = extra_count * abjad.Duration(1, 16)
-            if 0 < start_duration + extra_duration:
-                target_duration = start_duration + extra_duration
-            else:
-                target_duration = start_duration
-            numerators = []
-            for note in note_list:
-                duration = note.written_duration
-                fraction = abjad.NonreducedFraction(duration)
-                fraction = fraction.with_denominator(128)
-                numerators.append(fraction.numerator)
-            ratio = abjad.Ratio(numerators)
-            maker = rmakers.stack(
-                rmakers.tuplet([ratio]),
-                rmakers.rewrite_dots(),
-                rmakers.rewrite_sustained(),
-                rmakers.force_diminution(),
-            )
-            selection = maker([target_duration])
-            assert isinstance(selection, list)
-            inner_tuplet = selection[0]
-            if inner_tuplet.multiplier == 1:
-                inner_tuplet.hide = True
-            plts = baca.Selection(inner_tuplet).plts()
-            for j, plt in enumerate(plts):
-                source_note = note_list[j]
-                for pleaf in plt:
-                    pleaf.written_pitch = source_note.written_pitch
-                    voice_number = abjad.get.indicator(source_note, int)
-                    abjad.attach(voice_number, pleaf)
-            inner_tuplets.append(inner_tuplet)
-        return inner_tuplets
 
-    def _make_note_lists(self, segment_lists):
-        note_lists = []
-        for segment_list in segment_lists:
-            assert 0 < len(segment_list)
-            for segment in segment_list:
-                note_list = []
-                for number in segment:
-                    pitch_class = abjad.NumberedPitchClass(number)
-                    for operator in self.pc_operators:
-                        pitch_class = operator(pitch_class)
-                    note = abjad.Note(pitch_class, abjad.Duration(1, 4))
-                    note_list.append(note)
-                note_lists.append(note_list)
-        return note_lists
+def _displace_pitch_classes(music, pc_displacement):
+    if not pc_displacement:
+        return
+    notes = abjad.Selection(music).leaves(pitched=True)
+    total_notes = len(notes)
+    for i, note in enumerate(notes):
+        register = None
+        for pattern in pc_displacement:
+            if pattern.matches_index(i, total_notes):
+                register = "high"
+                break
+        else:
+            register = "low"
+        if register == "high":
+            pass
+        elif register == "low":
+            source_pitch = note.written_pitch
+            transposed_pitch = source_pitch.transpose(n=-12)
+            note.written_pitch = transposed_pitch
+        else:
+            raise ValueError(register)
 
-    def _make_rhythm(self):
-        segment_lists = self.segment_lists
-        assert isinstance(segment_lists, tuple)
-        note_lists = self._make_note_lists(segment_lists)
-        self._attach_voice_numbers(note_lists)
-        self._set_written_durations(note_lists)
-        inner_tuplets = self._make_inner_tuplets(note_lists)
-        return inner_tuplets
 
-    def _register_voices(self, music):
-        voice_1_registration = library.registrations["middle"]
-        voice_2_registration = library.registrations["low"]
-        voice_3_registration = library.registrations["lowest"]
-        for note in abjad.iterate.components(music, abjad.Note):
+def _make_inner_tuplets(note_lists, extra_counts):
+    extra_counts = extra_counts or [0]
+    extra_counts = abjad.CyclicTuple(extra_counts)
+    inner_tuplets = []
+    for i, note_list in enumerate(note_lists):
+        start_duration = sum(_.written_duration for _ in note_list)
+        extra_count = extra_counts[i]
+        extra_duration = extra_count * abjad.Duration(1, 16)
+        if 0 < start_duration + extra_duration:
+            target_duration = start_duration + extra_duration
+        else:
+            target_duration = start_duration
+        numerators = []
+        for note in note_list:
+            duration = note.written_duration
+            fraction = abjad.NonreducedFraction(duration)
+            fraction = fraction.with_denominator(128)
+            numerators.append(fraction.numerator)
+        ratio = abjad.Ratio(numerators)
+        maker = rmakers.stack(
+            rmakers.tuplet([ratio]),
+            rmakers.rewrite_dots(),
+            rmakers.rewrite_sustained(),
+            rmakers.force_diminution(),
+        )
+        selection = maker([target_duration])
+        assert isinstance(selection, list)
+        inner_tuplet = selection[0]
+        if inner_tuplet.multiplier == 1:
+            inner_tuplet.hide = True
+        plts = baca.Selection(inner_tuplet).plts()
+        for j, plt in enumerate(plts):
+            source_note = note_list[j]
+            for pleaf in plt:
+                pleaf.written_pitch = source_note.written_pitch
+                voice_number = abjad.get.indicator(source_note, int)
+                abjad.attach(voice_number, pleaf)
+        inner_tuplets.append(inner_tuplet)
+    return inner_tuplets
+
+
+def _make_note_lists(segment_lists, pc_operators):
+    note_lists = []
+    for segment_list in segment_lists:
+        assert 0 < len(segment_list)
+        for segment in segment_list:
+            note_list = []
+            for number in segment:
+                pitch_class = abjad.NumberedPitchClass(number)
+                for operator in pc_operators:
+                    pitch_class = operator(pitch_class)
+                note = abjad.Note(pitch_class, abjad.Duration(1, 4))
+                note_list.append(note)
+            note_lists.append(note_list)
+    return note_lists
+
+
+def _make_rhythm(segment_lists, voice_map, extra_counts, pc_operators):
+    note_lists = _make_note_lists(segment_lists, pc_operators)
+    _attach_voice_numbers(note_lists, voice_map)
+    _set_written_durations(note_lists)
+    inner_tuplets = _make_inner_tuplets(note_lists, extra_counts)
+    return inner_tuplets
+
+
+def _register_voices(music):
+    voice_1_registration = library.registrations["middle"]
+    voice_2_registration = library.registrations["low"]
+    voice_3_registration = library.registrations["lowest"]
+    for note in abjad.iterate.components(music, abjad.Note):
+        voice_number = abjad.get.indicator(note, int)
+        if voice_number == 1:
+            color = "#red"
+            abjad.override(note).accidental.color = color
+            abjad.override(note).beam.color = color
+            abjad.override(note).dots.color = color
+            abjad.override(note).note_head.color = color
+            abjad.override(note).repeat_tie.color = color
+            abjad.override(note).slur.color = color
+            abjad.override(note).stem.color = color
+            abjad.override(note).tie.color = color
+            registration = voice_1_registration
+        elif voice_number == 2:
+            registration = voice_2_registration
+        elif voice_number == 3:
+            color = "#blue"
+            abjad.override(note).accidental.color = color
+            abjad.override(note).beam.color = color
+            abjad.override(note).dots.color = color
+            abjad.override(note).note_head.color = color
+            abjad.override(note).repeat_tie.color = color
+            abjad.override(note).slur.color = color
+            abjad.override(note).stem.color = color
+            abjad.override(note).tie.color = color
+            registration = voice_3_registration
+        else:
+            raise ValueError(voice_number)
+        pitches = [note.written_pitch]
+        transposed_pitches = registration(pitches)
+        transposed_pitch = transposed_pitches[0]
+        note.written_pitch = transposed_pitch
+
+
+def _set_written_durations(note_lists):
+    for note_list in note_lists:
+        for note in note_list:
             voice_number = abjad.get.indicator(note, int)
             if voice_number == 1:
-                color = "#red"
-                abjad.override(note).accidental.color = color
-                abjad.override(note).beam.color = color
-                abjad.override(note).dots.color = color
-                abjad.override(note).note_head.color = color
-                abjad.override(note).repeat_tie.color = color
-                abjad.override(note).slur.color = color
-                abjad.override(note).stem.color = color
-                abjad.override(note).tie.color = color
-                registration = voice_1_registration
+                duration = abjad.Duration(1, 8)
             elif voice_number == 2:
-                registration = voice_2_registration
+                duration = abjad.Duration(1, 16)
             elif voice_number == 3:
-                color = "#blue"
-                abjad.override(note).accidental.color = color
-                abjad.override(note).beam.color = color
-                abjad.override(note).dots.color = color
-                abjad.override(note).note_head.color = color
-                abjad.override(note).repeat_tie.color = color
-                abjad.override(note).slur.color = color
-                abjad.override(note).stem.color = color
-                abjad.override(note).tie.color = color
-                registration = voice_3_registration
+                duration = abjad.Duration(1, 4)
             else:
                 raise ValueError(voice_number)
-            pitches = [note.written_pitch]
-            transposed_pitches = registration(pitches)
-            transposed_pitch = transposed_pitches[0]
-            note.written_pitch = transposed_pitch
-
-    def _set_written_durations(self, note_lists):
-        for note_list in note_lists:
-            for note in note_list:
-                voice_number = abjad.get.indicator(note, int)
-                if voice_number == 1:
-                    duration = abjad.Duration(1, 8)
-                elif voice_number == 2:
-                    duration = abjad.Duration(1, 16)
-                elif voice_number == 3:
-                    duration = abjad.Duration(1, 4)
-                else:
-                    raise ValueError(voice_number)
-                note.written_duration = duration
+            note.written_duration = duration
 
 
 #########################################################################################
@@ -199,21 +196,21 @@ class Maker:
 ### MUSIC-MAKERS ###
 
 music = []
-makers = []
 
 ### stage [1] (middle) ###
 
-maker = Maker(
+music_ = make_music(
     [1, 2, 0, -1, 5],
     library.pitch_classes[:6],
     [[2, range(0, 99)]],
     pc_displacement=[abjad.index(list(range(15)), 30)],
 )
-makers.append(maker)
+music.extend(music_)
+
 
 ### stage [2] (middle & lower) ###
 
-maker = Maker(
+music_ = make_music(
     [1, 2, 0, -1, 5],
     library.pitch_classes[2:8],
     [
@@ -223,11 +220,11 @@ maker = Maker(
     pc_displacement=[abjad.index(list(range(15)), 30)],
     pc_operators=[lambda _: _.transpose(n=1)],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [3] (lower) ###
 
-maker = Maker(
+music_ = make_music(
     [4, 8, 0, -4, 20],
     library.pitch_classes[4:6],
     [
@@ -238,11 +235,11 @@ maker = Maker(
         lambda _: _.invert(),
     ],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [4] (lower & upper) ###
 
-maker = Maker(
+music_ = make_music(
     [4, 8, 0, -4, 20],
     library.pitch_classes[6:8],
     [
@@ -253,11 +250,11 @@ maker = Maker(
         lambda _: _.transpose(n=3),
     ],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [5] (upper) ###
 
-maker = Maker(
+music_ = make_music(
     [2, 4, 0, -2, 10],
     library.pitch_classes[8:12],
     [
@@ -268,11 +265,11 @@ maker = Maker(
         lambda _: _.transpose(n=4),
     ],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [6] (upper & middle) ###
 
-maker = Maker(
+music_ = make_music(
     [2, 4, 0, -2, 10],
     library.pitch_classes[10:13],
     [
@@ -285,11 +282,11 @@ maker = Maker(
         lambda _: _.invert(),
     ],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [7] (upper, middle, lower) ###
 
-maker = Maker(
+music_ = make_music(
     [2, 4, 0, -2, 10],
     library.pitch_classes[12:20],
     [
@@ -327,11 +324,11 @@ maker = Maker(
     pc_displacement=[abjad.index(list(range(10, 20)), 20)],
     pc_operators=[lambda _: _.transpose(n=6)],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [8] (middle & lower) ###
 
-maker = Maker(
+music_ = make_music(
     [1, 2, 0, -1, 5],
     library.pitch_classes[14:18],
     [
@@ -342,11 +339,11 @@ maker = Maker(
         lambda _: _.transpose(n=7),
     ],
 )
-makers.append(maker)
+music.extend(music_)
 
 ### stage [9] (lower) ###
 
-maker = Maker(
+music_ = make_music(
     [4, 8, 0, -4, 20],
     library.pitch_classes[16:20],
     [
@@ -358,13 +355,7 @@ maker = Maker(
         lambda _: _.invert(),
     ],
 )
-makers.append(maker)
-
-### make music ###
-
-for maker in makers:
-    music_ = maker()
-    music.extend(music_)
+music.extend(music_)
 
 measure_durations = []
 current_duration = abjad.Duration(0)
@@ -386,8 +377,6 @@ for measure_duration in measure_durations:
 
 music_ = abjad.Selection(music)
 
-### SEGMENT-MAKER ###
-
 score = library.make_empty_score()
 voice_names = baca.accumulator.get_voice_names(score)
 
@@ -400,8 +389,6 @@ commands = baca.CommandAccumulator(
     voice_names=voice_names,
 )
 
-# skips
-
 commands(
     "Global_Skips",
     baca.metronome_mark(
@@ -409,8 +396,6 @@ commands(
         selector=baca.selectors.leaf(1 - 1),
     ),
 )
-
-# vc
 
 commands(
     ("vc", 1),
@@ -480,8 +465,6 @@ commands(
         baca.breathe(),
     ),
 )
-
-# rh
 
 commands(
     "rh",
