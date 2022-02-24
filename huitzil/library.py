4ca9c249a627ba9ed80b47d8c8c6d73599701dc0
Trevor Bača <trevor.baca@gmail.com>
2022-02-24 11:21:24 +0100

    Rerendered segments.

--- huitzil/library.py
+++ huitzil/library.py
@@ -20,7 +20,6 @@ pitch_classes = baca.accumulate_and_repartition(
     segments=[[6, 4, 5, 7, 8], [9, 3, 2, 4, 5], [1, 10, 9, 11, 0]],
     ratios=[(1,), (2, 3), (1, 4), (1,), (2, 3)],
     counts=[4, 2, 2, 3, 1],
-    tree=False,
 )
 
 registrations = dict(
