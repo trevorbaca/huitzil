4b42b9ffac8825d0a73f72555b9d8d2e559ace36
Trevor Bača <trevor.baca@gmail.com>
2022-04-13 15:49:00 +0200

    Rerendered sections.

--- huitzil/library.py
+++ huitzil/library.py
@@ -81,7 +81,7 @@ voice_abbreviations = {
 
 
 def make_empty_score():
-    tag = baca.function_name(inspect.currentframe())
+    tag = baca.tags.function_name(inspect.currentframe())
     global_context = baca.score.make_global_context()
     rh_music_voice = abjad.Voice(name="RH_Music_Voice", tag=tag)
     rh_music_staff = abjad.Staff(
