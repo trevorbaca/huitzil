815dacfbf735f2cc8899ebd9fb22cb78735564c4
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:40 -0400

    Cleaned up metadata.

--- huitzil/sections/05/music.py
+++ huitzil/sections/05/music.py
@@ -337,8 +337,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
