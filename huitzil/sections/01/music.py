1e0f640b9e1a54f5dd9eab906da5b86b8d1f82ff
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 06:17:45 -0400

    Cleaned up timing.

--- huitzil/sections/01/music.py
+++ huitzil/sections/01/music.py
@@ -443,8 +443,7 @@ def make_score():
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
-    score, measures = make_score(timing)
+    score, measures = make_score(environment.timing)
     metadata, persist = baca.section.postprocess_score(
         score,
         measures(),
@@ -460,7 +459,6 @@ def main():
         environment=environment,
         error_on_not_yet_pitched=True,
         manifests=library.manifests,
-        timing=timing,
     )
     lilypond_file = baca.lilypond.file(
         score,
@@ -473,7 +471,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 
