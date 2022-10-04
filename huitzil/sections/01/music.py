1b3ffa7212e97b1ac0dde7af1837825988b542da
Trevor Bača <trevor.baca@gmail.com>
2022-10-04 07:48:53 -0400

    Cleaned up music.py files.

--- huitzil/sections/01/music.py
+++ huitzil/sections/01/music.py
@@ -438,15 +438,14 @@ def make_score():
     )
     vc(cache["vc"])
     rh(cache["rh"])
-    return score, measures
+    return score
 
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    score, measures = make_score(environment.timing)
+    score = make_score(environment.timing)
     metadata = baca.section.postprocess_score(
         score,
-        measures(),
         **baca.section.section_defaults(),
         activate=[
             baca.tags.CLOCK_TIME,
