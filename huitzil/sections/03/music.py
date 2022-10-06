b33cc3d8047b0c618404bbfa8f7881076813506b
Trevor Bača <trevor.baca@gmail.com>
2022-10-06 07:35:56 -0400

    Cleaned up tags.

--- huitzil/sections/03/music.py
+++ huitzil/sections/03/music.py
@@ -374,13 +374,12 @@ def main():
         environment=environment,
         error_on_not_yet_pitched=True,
         manifests=library.manifests,
-        tags=baca.tags.Tags(
-            activate=[
-                # baca.tags.CLOCK_TIME,
-                baca.tags.LOCAL_MEASURE_NUMBER,
-                # baca.enums.MEASURE_NUMBER,
-            ],
-        ),
+    )
+    baca.tags.activate(
+        score,
+        # baca.tags.CLOCK_TIME,
+        baca.tags.LOCAL_MEASURE_NUMBER,
+        # baca.enums.MEASURE_NUMBER,
     )
     lilypond_file = baca.lilypond.file(
         score,
