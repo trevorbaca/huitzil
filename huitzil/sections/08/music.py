7a33be610b33e5db3682e929261742a3e48aa066
Trevor Bača <trevor.baca@gmail.com>
2023-04-02 13:49:10 -0400

    Cleaned up music.py files.

--- huitzil/sections/08/music.py
+++ huitzil/sections/08/music.py
@@ -347,7 +347,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
     GLOBALS(score["Skips"])
     VC(voices("vc"), time_signatures)
     RH(voices("rh"))
-    baca.section.reapply(
+    baca.section.reapply_persistent_indicators(
         voices,
         previous_persistent_indicators,
         manifests=library.manifests,
