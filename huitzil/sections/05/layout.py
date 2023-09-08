28d4e30ec062f27a62d748f51fcfc977edd3f5eb
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:51 -0400

    Cleaned up layout.py files.

--- huitzil/sections/05/layout.py
+++ huitzil/sections/05/layout.py
@@ -4,15 +4,19 @@ top = 17
 staves = (10, 15)
 separation = 70
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
-        baca.system(measure=10, y_offset=top + 1 * separation, distances=staves),
-        baca.system(measure=17, y_offset=top + 2 * separation, distances=staves),
-    ),
-    spacing=(1, 40),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
+            baca.system(measure=10, y_offset=top + 1 * separation, distances=staves),
+            baca.system(measure=17, y_offset=top + 2 * separation, distances=staves),
+        ),
+        spacing=(1, 40),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
--- huitzil/sections/08/layout.py
+++ huitzil/sections/08/layout.py
@@ -4,16 +4,20 @@ top = 17
 staves = (10, 15)
 separation = 57
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
-        baca.system(measure=7, y_offset=top + 1 * separation, distances=staves),
-        baca.system(measure=15, y_offset=top + 2 * separation, distances=staves),
-        baca.system(measure=23, y_offset=top + 3 * separation, distances=staves),
-    ),
-    spacing=(1, 40),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
+            baca.system(measure=7, y_offset=top + 1 * separation, distances=staves),
+            baca.system(measure=15, y_offset=top + 2 * separation, distances=staves),
+            baca.system(measure=23, y_offset=top + 3 * separation, distances=staves),
+        ),
+        spacing=(1, 40),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
