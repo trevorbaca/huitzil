4eac9b83598e5cf34e9493fdb7b51d95e7b0612c
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:44 -0400

    Cleaned up music.py files.

--- huitzil/sections/07/music.py
+++ huitzil/sections/07/music.py
@@ -294,5 +294,19 @@ def main():
     )
 
 
+def make_layout():
+    top = 17
+    staves = (8, 22)
+    separation = 70
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
+        ),
+        spacing=(1, 28),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
--- huitzil/sections/09/music.py
+++ huitzil/sections/09/music.py
@@ -352,5 +352,20 @@ def main():
     )
 
 
+def make_layout():
+    top = 17
+    staves = (10, 17)
+    separation = 70
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=top + 0 * separation, distances=staves),
+            baca.system(measure=12, y_offset=top + 1 * separation, distances=staves),
+        ),
+        spacing=(1, 32),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
