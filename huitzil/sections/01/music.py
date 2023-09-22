f6ee714041e1b139612caf6185e613ed84552e31
Trevor Bača <trevor.baca@gmail.com>
2023-09-22 14:32:30 -0400

    Cleaned up music.py files.

--- huitzil/sections/01/music.py
+++ huitzil/sections/01/music.py
@@ -427,7 +427,6 @@ def make_score():
         score,
         time_signatures(),
         append_anchor_skip=True,
-        always_make_global_rests=True,
         manifests=library.manifests,
     )
     GLOBALS(score["Skips"])
@@ -448,7 +447,6 @@ def persist_score(score, environment):
         score,
         environment,
         library.manifests,
-        always_make_global_rests=True,
         do_not_require_short_instrument_names=True,
         error_on_not_yet_pitched=True,
         first_section=True,
