ae6df129d646295bf0ab0ff6e00f9c35676e53bb
Trevor Bača <trevor.baca@gmail.com>
2025-10-15 11:05:38 -0400

    Clean up calls to rmakers.toggle_augmented_tuplets()

--- source/huitzil/sections/01/music.py
+++ source/huitzil/sections/01/music.py
@@ -98,7 +98,7 @@ def _make_inner_tuplets(note_lists, extra_counts):
         time_signatures = [abjad.TimeSignature(target_duration.pair())]
         voice = rmakers.wrap_in_time_signature_staff(tuplets, time_signatures)
         rmakers.respell_tuplets_without_dots(tuplets)
-        rmakers.force_diminution(tuplets)
+        rmakers.toggle_augmented_tuplets(tuplets)
         rmakers.rewrite_sustained_tuplets(tuplets)
         components = abjad.mutate.eject_contents(voice)
         assert isinstance(components, list)
